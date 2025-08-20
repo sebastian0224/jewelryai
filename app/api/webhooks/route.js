bc0603a15d92b38687bb8079b3365cb521a2cb5b
sebastian0224 <sebastianosoriocarrasquilla@gmail.com>
2025-08-20 10:49:53 -0500

    fix prisma import

--- app/api/webhooks/route.js
+++ app/api/webhooks/route.js
@@ -1,6 +1,6 @@
 // app/api/webhooks/clerk/route.js
 import { verifyWebhook } from "@clerk/nextjs/webhooks";
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 export async function POST(req) {
   try {
